92ee97de3bc02503ca06ea20783f327877f08766
github username <github email address>
2019-11-08 11:02:25 -0500

    added method names for prediction learner model

---
+++ src/PredictionLearner.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class PredictionLearner {
+	/**
+	 * A learner class that aims to learn from past predictions
+	 * Adjust weight of similarities based on prediction feedbacks
+	 */
+	
+	ArrayList<Prediction> predictions;
+	
+	public void makePrediction(User user) {
+		// calls on similarity calculator class
+	}
+	
+	public void makeWeightAdjustment(User user) {
+		
+	}
+}
