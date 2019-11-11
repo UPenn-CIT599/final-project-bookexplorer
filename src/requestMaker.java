90ece34844c551b3907ceb00166286d64c68dc39
Emily Y <tong.zhao.92@gmail.com>
2019-11-11 16:19:09 -0500

    Added more body to getAuthor method

--- src/requestMaker.java
+++ src/requestMaker.java
@@ -3,26 +3,34 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Scanner;
 
-public class requestMaker {
+public class RequestMaker {
 	
 	/**
-	 * Responsibility: makes http request to goodreads API and interprets responses
+	 * Responsibility: makes http request to goodreads API and parses XML responses
 	 */
 	
 	String authorAPIUrl;
 	String developerKey;
 	
-	public requestMaker() {
-		this.authorAPIUrl = "https://www.goodreads.com/api/author_url";
+	public RequestMaker() {
+		this.authorAPIUrl = "https://www.goodreads.com/api/author_url/";
 		//TODO update below to be the real developer key
 		this.developerKey = "key";
 	}
 	
-	public void getAuthor(String authorName) throws MalformedURLException, IOException {
+	// https://stackoverflow.com/questions/2793150/how-to-use-java-net-urlconnection-to-fire-and-handle-http-requests
+	public String getAuthor(String authorName) throws MalformedURLException, IOException {
 		String query = String.format("key=%s", this.developerKey);
-		// https://stackoverflow.com/questions/2793150/how-to-use-java-net-urlconnection-to-fire-and-handle-http-requests
-		URLConnection connection = new URL(authorAPIUrl + "?" + query).openConnection();
+		URLConnection connection = new URL(authorAPIUrl + authorName + "?" + query).openConnection();
 		InputStream response = connection.getInputStream();
+		Scanner respScanner = new Scanner(response);
+		String respBody = respScanner.next();
+		return respBody;
+	}
+
+	private boolean isAuthorFound(String respBody) {
+
 	}
 }
