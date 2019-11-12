fa625383d6c6bd2f0c0d8475693f762907f4ba4e
Emily Y <tong.zhao.92@gmail.com>
2019-11-11 19:25:48 -0500

    Added details in user interactions

--- src/UserInteraction.java
+++ src/UserInteraction.java
@@ -7,11 +7,16 @@ public class UserInteraction {
 	 */
 
 	public static void main(String[] args) {
-		// To user: Please provide 3 books that you like
-		// To user: please provide 3 authors that you enjoy reading
-		// Run prediction logic
+		// To user: ask for favorite genre - from a dropdown menu of genres
+		// (We have a list of authors for each genre)
+		// To user: Please provide 1 to 3 books that you like
+		// To user: please provide 1 to 3 authors that you enjoy reading
+		// Run similarity metrics of authors the user input, with our list of authors
+		// pick an author recommended by our algo
+		// pick a book by the author recommended
 		// Return recommended author, with image and short description
 		// Return recommended author's book, with image and short description, amazon link, etc.
+		// Ask for user feedback - if not good, adjust weight of similarity metrics, then give another one
 	}
 
 }
