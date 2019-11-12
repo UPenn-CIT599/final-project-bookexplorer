27a2d8e869ac7ed780186ebae27043b8656cd9fe
Emily Y <tong.zhao.92@gmail.com>
2019-11-12 09:57:12 -0500

    Updated user interaction outline

--- src/UserInteraction.java
+++ src/UserInteraction.java
@@ -7,16 +7,14 @@ public class UserInteraction {
 	 */
 
 	public static void main(String[] args) {
-		// To user: ask for favorite genre - from a dropdown menu of genres
-		// (We have a list of authors for each genre)
-		// To user: Please provide 1 to 3 books that you like
-		// To user: please provide 1 to 3 authors that you enjoy reading
+		// To user: select a genre from dropdown menu that they recently read
+		// To user: Please provide an author that you most recently read and enjoyed
+		// To user: please provide a book that you most recently read and enjoyed
 		// Run similarity metrics of authors the user input, with our list of authors
 		// pick an author recommended by our algo
-		// pick a book by the author recommended
+		// pick a book by the author similar to the book that user provided
 		// Return recommended author, with image and short description
 		// Return recommended author's book, with image and short description, amazon link, etc.
 		// Ask for user feedback - if not good, adjust weight of similarity metrics, then give another one
 	}
-
 }
