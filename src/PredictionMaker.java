b919d99b43c2dbb279c24dcdbf55edc469d0e142
Emily Y <tong.zhao.92@gmail.com>
2019-11-12 16:42:38 -0500

    Small changes to author prediction

--- src/PredictionMaker.java
+++ src/PredictionMaker.java
@@ -1,19 +1,42 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class PredictionMaker {
 	/**
 	 * Responsibility: makes author and book predictions for the user
 	 * Collaborator: UserInteraction, WebScraper, Book, Author
 	 */
-	
+
+
+
+	HashMap<String, ArrayList<String>> genreAuthors;
+	RequestHandler handler;
+
+	public PredictionMaker() {
+		//TODO - add csv file with author - genre pairs and read from csv file
+		ArrayList<String> yaAuthors = new ArrayList(Arrays.asList("JK Rowling"));
+		this.genreAuthors = new HashMap<String, ArrayList<String>>() {{
+			put("Young Adult", yaAuthors);
+		}};
+	}
 
 	/**
 	 * predicts an author that the user might like
-	 * @param author name from user input
+	 * @param authorFromUser author name string from user input
+	 * @param genre genre string from user input
 	 * @return an author object
 	 */
-	public Author getAuthorPrediction(String authorFromUser) {
-		return "Stephen King";
+	public Author getAuthorPrediction(String authorFromUser, String genre) {
+		ArrayList<String> authorNames = genreAuthors.get(genre);
+		double maxSimilarity = 0;
+		String mostSimilarAuthor = "";
+		for (String name : authorNames) {
+//			Author newAuthor = handler.getAuthor(name);
+			//TODO: add process to run similarity metrics between 2 authors
+			mostSimilarAuthor = name;
+		}
+		return new Author(mostSimilarAuthor);
 	}
 	
 	/**
@@ -22,7 +45,7 @@ public class PredictionMaker {
 	 * @return book prediction
 	 */
 	public Book getBookPrediction(Author author) {
-		
+		return new Book();
 	}
 	
 	/**
