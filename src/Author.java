3e6c2e8b21857a9c571411c159265588f52386c1
github username <github email address>
2019-11-18 14:09:39 -0500

    added author attributes

--- src/Author.java
+++ src/Author.java
@@ -17,7 +17,13 @@ import java.util.ArrayList;
  */
 public class Author {
 
-	String name;
+	public String name;
+	public String description;
+	public String goodReadsID;
+	public int worksCount;
+	public String genre;
+	public ArrayList<Book> books;
+
 
 	public Author(String name) {
 		this.name = name;
