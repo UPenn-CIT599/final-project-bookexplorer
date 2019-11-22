14c284a59cb90b22a1a6f93c6f9a6eead83f4608
emilyyz92 <github email address>
2019-11-22 10:21:46 -0500

    added documentations to author prediction method

--- src/main/java/RecMaker.java
+++ src/main/java/RecMaker.java
@@ -16,15 +16,14 @@ public class RecMaker {
 	HashMap<String, ArrayList<String>> genreAuthors;
 	RequestHandler handler;
 	HashMap<String, Author> seenAuthorsByID;
-	HashMap<String, Author> seenBooksByID;
+	HashMap<String, Book> seenBooksByID;
 
-	public RecMaker() {
+	public RecMaker(String fileName) {
 		//TODO - add csv file with author - genre pairs and read from csv file
 		final ArrayList<String> yaAuthors = new ArrayList(Arrays.asList("JK Rowling"));
-		this.genreAuthors = new HashMap<String, ArrayList<String>>() {{
-			put("Young Adult", yaAuthors);
-		}};
+		this.genreAuthors = readGenreAuthorsFile(fileName);
 		this.seenAuthorsByID = new HashMap<String, Author>();
+		this.seenBooksByID = new HashMap<String, Book>();
 	}
 
 	/**
@@ -35,17 +34,22 @@ public class RecMaker {
 	 */
 	public Author getAuthorPrediction(String authorID, String genre) throws ParserConfigurationException, SAXException, IOException {
 		Author currentAuthor;
+		// check if author is seen, if yes, get author, if not, get author information by pulling data from GoodReads API, and save it to seenAuthors hash map
 		if (!seenAuthorsByID.containsKey(authorID)) {
 			currentAuthor = handler.saveAuthorDetails(authorID);
+			seenAuthorsByID.put(currentAuthor.goodReadsID, currentAuthor);
 		} else {
 			currentAuthor = seenAuthorsByID.get(authorID);
 		}
 		Author mostSimilarAuthor = currentAuthor;
+		// get list of authors from the same genre
 		ArrayList<String> authorIDs = genreAuthors.get(genre);
 		double maxSimilarity = 0;
+		// find author of the max weighted similarity
 		for (String id : authorIDs) {
 			Author compareAuthor = seenAuthorsByID.get(id);
-			double similarity = authorSimilarity(currentAuthor, compareAuthor);
+			AuthorSimilarity simCalculator = new AuthorSimilarity(currentAuthor, compareAuthor);
+			double similarity = simCalculator.weightedSimilarity();
 			if (similarity > maxSimilarity) {
 				maxSimilarity = similarity;
 				mostSimilarAuthor = compareAuthor;
@@ -53,17 +57,13 @@ public class RecMaker {
 		}
 		return mostSimilarAuthor;
 	}
-
-	public double authorSimilarity(Author author1, Author author2) {
-		return 0.0;
-	}
 	
 	/**
 	 * predicts book based on author prediction
 	 * @param author predicted by getAuthorPrediction
 	 * @return book prediction
 	 */
-	public Book getBookPrediction(Author author) {
+	public Book getBookPrediction(Author author, String bookTitle) {
 		for (Book book : author.books) {
 
 		}
@@ -71,7 +71,7 @@ public class RecMaker {
 	}
 
 	/**
-	 *
+	 * read data file that contains genre -> author names
 	 * @param fileName
 	 * @return
 	 */
