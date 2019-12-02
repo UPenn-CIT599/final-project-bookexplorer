37692a87111cbba72b90a61627193a04eb933ab1
emilyyz92 <github email address>
2019-12-02 12:32:37 -0500

    refactored book similarity calculator and added documentation and test skeleton

--- src/main/java/BookSimCalculator.java
+++ src/main/java/BookSimCalculator.java
@@ -2,6 +2,14 @@ import java.util.HashMap;
 
 public class BookSimCalculator {
 
+    /**
+     * Responsibilities:
+     * Calculates Similarities between 2 books objects, based on the books' attributes
+     * Equal weights are distributed to each attribute, unless otherwise specified
+     * Collaborator:
+     * Book
+     */
+
     Book targetBook;
     Book comparedBook;
     HashMap<String, Double> weights;
@@ -10,19 +18,12 @@ public class BookSimCalculator {
         this.targetBook = targetBook;
         this.comparedBook = comparedBook;
         this.weights = new HashMap<String, Double>() {{
-            put("ratings", 0.25);
-            put("ratingsCount", 0.25);
-            put("bookLength", 0.25);
-            put("description", 0.25);
+            put("ratings", 0.2);
+            put("ratingsCount", 0.2);
+            put("bookLength", 0.2);
+            put("description", 0.2);
+            put("publicationYear", 0.2);
         }};
-    }
-
-    public double ratingSim() {
-        return ((double) targetBook.averageRating) / comparedBook.averageRating;
-    }
-
-    public double ratingsCountSim() {
-        return (double) targetBook.ratingsCount / comparedBook.ratingsCount;
     }
 
     public double descriptionSim() {
@@ -30,11 +31,30 @@ public class BookSimCalculator {
         return 1.0;
     }
 
-    public double bookLengthSim() {
-        return (double) targetBook.pagesNum / comparedBook.pagesNum;
+    /**
+     * returns 0 if 2 attributes are most similar, and 1 if least similar
+     * @param num1 - attribute from first book
+     * @param num2 - attribute from second book
+     * @return
+     */
+    private double singleSimCalc(double num1, double num2) {
+        if (num1 == 0 && num2 == 0) {
+            return 1;
+        } else {
+            return Math.abs(num1 - num2) / (num1 + num2);
+        }
     }
 
+    /**
+     * This method combines the similarities of 2 books' attributes by weights and returns 1 if most similar, and 0 if least similar
+     * @return weighted similarity between 2 books
+     */
     public double weightedSimilarity() {
-        return weights.get("ratings") * ratingSim() + weights.get("description") * descriptionSim() + weights.get("ratingsCount") * ratingsCountSim() + weights.get("bookLength") * bookLengthSim();
+        double ratingsSim = weights.get("ratings") * singleSimCalc(comparedBook.averageRating, targetBook.averageRating);
+        double ratingsCountSim = weights.get("ratingsCount") * singleSimCalc(comparedBook.ratingsCount, targetBook.ratingsCount);
+        double publicationYearSim = weights.get("publicationYear") * singleSimCalc(comparedBook.publicationYear, targetBook.publicationYear);
+        double bookLengthSim = weights.get("bookLength") * singleSimCalc(comparedBook.pagesNum, targetBook.pagesNum);
+        double descriptionSim = weights.get("description") * descriptionSim();
+        return 1 - (ratingsSim + ratingsCountSim + publicationYearSim + bookLengthSim + descriptionSim);
     }
 }
--- src/test/java/BookSimCalculatorTest.java
+++ src/test/java/BookSimCalculatorTest.java
@@ -7,22 +7,18 @@ class BookSimCalculatorTest {
 
     @BeforeEach
     void setUp() {
-    }
-
-    @Test
-    void ratingSim() {
-    }
-
-    @Test
-    void ratingsCountSim() {
+        Book book1 = new Book("Pride and Prejudice");
+        book1.goodReadsID = "3060926";
+        book1.ratingsCount = 2676768;
+        book1.publicationYear = 1813;
+        book1.averageRating = 4.26;
+        Book book2 = new Book("Sense and Sensibility");
+        Book book3 = new Book("Tipping Point");
     }
 
     @Test
     void descriptionSim() {
-    }
 
-    @Test
-    void bookLengthSim() {
     }
 
     @Test
