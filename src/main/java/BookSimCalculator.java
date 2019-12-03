3f26bfb0d5f2cb78ad81b0e6a24c5e4c1c274c92
emilyyz92 <github email address>
2019-12-02 19:59:37 -0500

    added default constructor for inheritance

--- src/main/java/BookSimCalculator.java
+++ src/main/java/BookSimCalculator.java
@@ -26,6 +26,10 @@ public class BookSimCalculator {
         }};
     }
 
+    public BookSimCalculator() {
+
+    }
+
     public double descriptionSim() {
         //TODO update below
         return 1.0;
@@ -37,7 +41,7 @@ public class BookSimCalculator {
      * @param num2 - attribute from second book
      * @return
      */
-    private double singleSimCalc(double num1, double num2) {
+    public double singleSimCalc(double num1, double num2) {
         if (num1 == 0 && num2 == 0) {
             return 1;
         } else {
