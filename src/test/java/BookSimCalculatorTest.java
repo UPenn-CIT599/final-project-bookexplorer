0f896607b375394ee10e74a049d09edaf73c18b7
emilyyz92 <github email address>
2019-12-02 19:59:58 -0500

    added weighted similarity method unit test

--- src/test/java/BookSimCalculatorTest.java
+++ src/test/java/BookSimCalculatorTest.java
@@ -5,15 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BookSimCalculatorTest {
 
-    @BeforeEach
+    @org.junit.jupiter.api.BeforeEach
     void setUp() {
-        Book book1 = new Book("Pride and Prejudice");
-        book1.goodReadsID = "3060926";
-        book1.ratingsCount = 2676768;
-        book1.publicationYear = 1813;
-        book1.averageRating = 4.26;
-        Book book2 = new Book("Sense and Sensibility");
-        Book book3 = new Book("Tipping Point");
+
     }
 
     @Test
@@ -23,5 +17,26 @@ class BookSimCalculatorTest {
 
     @Test
     void weightedSimilarity() {
+        Book book1 = new Book("Pride and Prejudice");
+        book1.goodReadsID = "1885";
+        book1.ratingsCount = 2676768;
+        book1.publicationYear = 1813;
+        book1.averageRating = 4.26;
+        book1.pagesNum = 279;
+        Book book2 = new Book("Sense and Sensibility");
+        book2.goodReadsID = "14935";
+        book2.ratingsCount = 888581;
+        book2.publicationYear = 1811;
+        book2.averageRating = 4.07;
+        book2.pagesNum = 409;
+        Book book3 = new Book("Tipping Point");
+        book3.goodReadsID = "2612";
+        book3.ratingsCount = 627591;
+        book3.publicationYear = 2000;
+        book3.averageRating = 3.96;
+        book3.pagesNum = 301;
+        BookSimCalculator cal2 = new BookSimCalculator(book1, book2);
+        BookSimCalculator cal1 = new BookSimCalculator(book2, book3);
+        assertEquals(true,cal1.weightedSimilarity() > cal2.weightedSimilarity());
     }
 }
